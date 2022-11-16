1722a0a046d324d448455dca718f53231c3504b7
milliorn <scottmilliorn@gmail.com>
2022-11-16 02:24:39 -0800

    header, img

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,61 +11,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.body}>
+        <header className={styles.header}>
+          <nav>
+            <ul>
+              <li>
+                <button className={styles.btn_login}>Log In</button>
+              </li>
+            </ul>
+          </nav>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+          <div className={styles.header_content}>
+            <h4>Try up to one month free</h4>
+            {/** https://stackoverflow.com/a/73618982/11986604 */}
+            <Image
+              src="/../public/img/logo.png"
+              alt="Hulu logo"
+              className="logo"
+              height="0"
+              width="270"
+              style={{ width: "auto", height: "auto", margin: "20px 0" }}
+            />
+            <div className={styles.header_text_1}>
+              Watch thousands of TV shows and movies.
+            </div>
+            <div className={styles.header_text_2}>
+              HBO Max™, SHOWTIME®, CINEMAX® and STARZ® available as add-ons.
+            </div>
+            <button className={styles.btn_cta}>Start Your Free Trial</button>
+            <div className={styles.legal_text}>
+              Free trial for new & eligible returning subscribers only.
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
