03b5fb04ef26465b0e7bfd4ae0ef61db182f0732
milliorn <scottmilliorn@gmail.com>
2022-11-18 00:45:58 -0800

    categories

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,6 +72,46 @@ export default function Home() {
             </a>
           </div>
         </section>
+
+        <section className={styles.categories}>
+          <h4>Included in all plans</h4>
+          <div className={styles.text_xl}>All The TV You Love</div>
+          <div className={styles.sub_text}>
+            Stream full seasons of exclusive series, current-season episodes,
+            hit movies, Hulu Originals, kids shows, and more.
+          </div>
+
+          <div className={styles.covers}>
+            <div className={styles.cover_1}>
+              <div className={styles.cover_grid}></div>
+              <div className={styles.cover_text}>
+                <div className="sub-title">Past & Current Seasons</div>
+                <h3>TV Shows</h3>
+              </div>
+            </div>
+            <div className={styles.cover_2}>
+              <div className={styles.cover_grid}></div>
+              <div className={styles.cover_text}>
+                <div className="sub-title">New & Classics</div>
+                <h3>Movies</h3>
+              </div>
+            </div>
+            <div className={styles.cover_3}>
+              <div className={styles.cover_grid}></div>
+              <div className={styles.cover_text}>
+                <div className="sub-title">Groundbreaking</div>
+                <h3>Hulu Originals</h3>
+              </div>
+            </div>
+            <div className={styles.cover_4}>
+              <div className={styles.cover_grid}></div>
+              <div className={styles.cover_text}>
+                <div className="sub-title">Add-On</div>
+                <h3>Premiums</h3>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
