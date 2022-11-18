b350a04feef3d286d347ce47ce3b8858c5008a27
Scott Milliorn <scottmilliorn@gmail.com>
2022-11-18 01:15:54 -0800

    Class live (#33)
    * live
    
    * live_border
    
    * text_xl
    
    * legal_text

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,6 +112,23 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className={styles.live}>
+          <div className={styles.live_border}>
+            <h4>Hulu + Live TV</h4>
+            <div className={styles.text_xl}>Live TV Makes It Better</div>
+            <div className={styles.sub_text}>
+              Make the switch from cable. Get 75+ top channels with your
+              favorite live sports, news, and events - plus the entire Hulu
+              streaming library.
+            </div>
+            <div className={styles.legal_text}>
+              Live TV plan required. Regional restrictions, blackouts and
+              additional terms apply. See details
+            </div>
+            <a href="#">View Channels in Your Area</a>
+          </div>
+        </section>
       </div>
     </div>
   );
