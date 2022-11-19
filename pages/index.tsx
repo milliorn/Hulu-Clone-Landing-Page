cf2061bc16bf6dabd9b120926fe2a9542a09a7d3
milliorn <scottmilliorn@gmail.com>
2022-11-18 22:34:21 -0800

    className

--- pages/index.tsx
+++ pages/index.tsx
@@ -150,32 +150,28 @@ export default function Home() {
                 <Image
                   alt="cbs sports logo"
                   src={logo1}
-                  style={{ width: "auto", height: "auto" }}
-                  width="40"
+                  style={{ width: "40px", height: "auto" }}
                 />
               </div>
               <div>
                 <Image
                   alt="espn logo"
                   src={logo2}
-                  style={{ width: "auto", height: "auto" }}
-                  width="40"
+                  style={{ width: "40px", height: "auto" }}
                 />
               </div>
               <div>
                 <Image
                   alt="nbc logo"
                   src={logo3}
-                  style={{ width: "auto", height: "auto" }}
-                  width="40"
+                  style={{ width: "40px", height: "auto" }}
                 />
               </div>
               <div>
                 <Image
                   src={logo4}
                   alt="nbc golf logo"
-                  style={{ width: "auto", height: "auto" }}
-                  width="40"
+                  style={{ width: "40px", height: "auto" }}
                 />
               </div>
             </div>
@@ -185,6 +181,57 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-lists">
+          <ul>
+            <li className="list-head">BROWSE</li>
+            <li><a href="#">Streaming Library</a></li>
+            <li><a href="#">Live TV</a></li>
+            <li><a href="#">Live News</a></li>
+            <li><a href="#">Live Sports</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">TV Shows</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Originals</a></li>
+            <li><a href="#">Networks</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">FX on Hulu</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">Hulu, Disney+, and ESPN+</a></li>
+            <li><a href="#">Disney Bundle</a></li>
+            <li><a href="#">HBO Max</a></li>
+            <li><a href="#">Cinimax</a></li>
+            <li><a href="#">Showtime</a></li>
+            <li><a href="#">STARZ</a></li>
+          </ul>
+          <ul>
+            <li className="list-head">HELP</li>
+            <li><a href="#">Account & Billing</a></li>
+            <li><a href="#">Plans & Pricing</a></li>
+            <li><a href="#">Supported Devices</a></li>
+            <li><a href="#">Accesibility</a></li>
+          </ul>
+          <ul>
+            <li className="list-head">ABOUT US</li>
+            <li><a href="#">Press</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div className="divider"></div>
+
+        <div className="social-icons">
+          <a href="#"><img src="img/facebook.svg" alt="" /></a>
+          <a href="#"><img src="img/twitter.svg" alt="" /></a>
+          <a href="#"><img src="img/youtube.svg" alt="" /></a>
+          <a href="#"><img src="img/instagram.svg" alt="" /></a>
+        </div>
+      </div>
+    </footer>
       </div>
     </div>
   );
