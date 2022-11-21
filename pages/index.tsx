1f246714929ecc0a772c8046acba33a3898d2a63
milliorn <scottmilliorn@gmail.com>
2022-11-20 20:27:40 -0800

    InnerModal

--- pages/index.tsx
+++ pages/index.tsx
@@ -390,53 +390,77 @@ export default function Home() {
         /** https://stackoverflow.com/a/50625073/11986604 */
         ariaHideApp={false}
       >
-        <div className="modal" id="modal">
-          <div className={styles.modal_box}>
-            <div className={styles.modal_body}>
-              <h3 className={styles.modal_body_h3}>Log In</h3>
-              <form>
-                <div className={styles.form_control}>
-                  <label htmlFor="email">Email</label>
-                  <input type="email" id="email" />
-                </div>
-                <div className={styles.form_control}>
-                  <label htmlFor="password">Password</label>
-                  <input type="password" id="password" />
-                </div>
-                <p>
-                  <a href="#">Forgot your email or password</a>
-                </p>
-                <button type="button" className={styles.btn_dark}>
-                  Log In
-                </button>
-              </form>
-            </div>
-            <div className={styles.modal_footer}>
-              <p>Don&apos;t have an account?</p>
-              <a
-                href="#"
-                style={{
-                  lineHeight: "2rem",
-                }}
-              >
-                Start your free trial
-              </a>
-            </div>
-            <Image
-              alt="close"
-              className={styles.close}
-              onClick={closeModal}
-              src={close}
-              style={{
-                cursor: "pointer",
-                position: "absolute",
-                left: "0.5rem",
-                bottom: "0.5rem",
-              }}
-            />
-          </div>
-        </div>
+        <InnerModal />
       </Modal>
     </div>
   );
+
+  function InnerModal(): JSX.Element {
+    return (
+      <div className="modal" id="modal">
+        <div className={styles.modal_box}>
+          <InnerModalBody />
+          <InnerModalFooter />
+          <Image
+            alt="close"
+            className={styles.close}
+            onClick={closeModal}
+            src={close}
+            style={{
+              cursor: "pointer",
+              position: "absolute",
+              left: "0.5rem",
+              bottom: "0.5rem",
+            }}
+          />
+        </div>
+      </div>
+    );
+
+    function InnerModalFooter(): JSX.Element {
+      return (
+        <div className={styles.modal_footer}>
+          <p>Don&apos;t have an account?</p>
+          <a
+            href="#"
+            style={{
+              lineHeight: "2rem",
+            }}
+          >
+            Start your free trial
+          </a>
+        </div>
+      );
+    }
+
+    function InnerModalBody(): JSX.Element {
+      return (
+        <div className={styles.modal_body}>
+          <h3 className={styles.modal_body_h3}>Log In</h3>
+          <InnerModalBodyForm />
+        </div>
+      );
+
+      function InnerModalBodyForm(): JSX.Element {
+        return (
+          <form>
+            <div className={styles.form_control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={styles.form_control}>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <p>
+              <a href="#">Forgot your email or password</a>
+            </p>
+            <button type="button" className={styles.btn_dark}>
+              Log In
+            </button>
+          </form>
+        );
+      }
+    }
+  }
 }
