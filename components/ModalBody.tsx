5f8c7c0ea42d47f5021078a7e12f2b6f6be6ce1d
milliorn <scottmilliorn@gmail.com>
2022-11-21 01:37:29 -0800

    ModalForm

--- components/ModalBody.tsx
+++ components/ModalBody.tsx
@@ -1,26 +1,12 @@
 import styles from "../styles/Home.module.css";
+import { ModalForm } from "./ModalForm";
 
 // inner modal body with form and button
 export function ModalBody(): JSX.Element {
   return (
     <div className={styles.modal_body}>
       <h3 className={styles.modal_body_h3}>Log In</h3>
-      <form>
-        <div className={styles.form_control}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-        </div>
-        <div className={styles.form_control}>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <p>
-          <a href="#">Forgot your email or password</a>
-        </p>
-        <button type="button" className={styles.btn_dark}>
-          Log In
-        </button>
-      </form>
+      <ModalForm />
     </div>
   );
 }
